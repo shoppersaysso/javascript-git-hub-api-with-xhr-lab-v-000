1163864207a4619b6e4671b0589743977eb0265e
Sara Hopper <sara.hopper@gmail.com>
2017-09-10 19:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,10 +26,14 @@ function displayRepositories(event, data) {
   }
 
 function getCommits(el) {
-  const name = el.dataset.repo
-
-}
-
+  const repoName = el.dataset.repository
+    const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+    const xhr = new XMLHttpRequest()
+    xhr.addEventListener("load", displayCommits)
+    xhr.open("GET", uri)
+    xhr.send()
+  }
+  
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' +
