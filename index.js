ce034c46d0e2286bf7d4a7e1af2167c2f7fa2ce3
Sara Hopper <sara.hopper@gmail.com>
2017-09-10 19:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function displayRepositories(event, data) {
 
 function getCommits(el) {
   const repoName = el.dataset.repository
-  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+  const uri = "https://api.github.com/" + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
   const xhr = new XMLHttpRequest()
   xhr.addEventListener("load", displayCommits)
   xhr.open("GET", uri)
@@ -36,7 +36,7 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + 
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name +
 ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
