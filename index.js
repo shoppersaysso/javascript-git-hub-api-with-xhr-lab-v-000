f2ae443f4a929fc0ced035aa85d11210f3b6cef3
Sara Hopper <sara.hopper@gmail.com>
2017-09-10 19:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function showRepositories(event, data) {
 function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits)
+  req.addEventListener("load", displayCommits)
   req.open("GET",
   'https://api.github.com/repos/shoppersaysso/' + name + '/commits')
   req.send()
