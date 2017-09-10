267b78205f80f5d9d18d650fcd629ecffdd90859
Sara Hopper <sara.hopper@gmail.com>
2017-09-10 19:32:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ function getRepositories() {
 
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="'+ r.name +
-'"onclick="getCommits(this)">Get Commits</a></li>').join('')}<ul>`
-  document.getElementById("repositories").innerHTML = repoList
-}
+  const repoList = "<ul>" + repos.map(repo => {
+      const dataUsername = 'data-username="' + repo.owner.login + '"'
+      const dataRepoName = 'data-repository="' + repo.name + '"'
+      return(`
+            <li>
+              <h2>${repo.name}</h2>
+              <a href="${repo.html_url}">${repo.html_url}</a><br>
+              <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+              <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+            </li>`
+            )
+    }).join('') + "</ul>";
+    document.getElementById("repositories").innerHTML = repoList
+  }
 
 function getCommits(el) {
   const name = el.dataset.repo
